fd378d0c6dee686385fcafea664f93f7f75fb544
Pugonly <410421242@ems.ndhu.edu.tw>
2018-05-03 22:11:38 +0800

    a

---
+++ assignment.py
@@ -0,0 +1,11 @@
+from PIL import Image
+import numpy as np
+import scipy.misc
+
+
+key1 = Image.open("key1.png")
+key2 = Image.open("key2.png")
+Eprime = Image.open("Eprime.png")
+I = Image.open("I.png")
+
+
