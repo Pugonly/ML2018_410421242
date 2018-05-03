efc44723a6ed8e584b21324dfab9eef667f50a34
Pugonly <410421242@ems.ndhu.edu.tw>
2018-05-03 23:46:39 +0800

    a

--- assignment.py
+++ assignment.py
@@ -24,3 +24,21 @@ for h in range(300):
         temp[h*400+w]=arrEprime[h][w]
 
 
+x=np.array([0,0,0])
+maxlimit=10
+temp_x0=10
+temp_x1=10
+temp_x2=10
+
+epoch=1
+while epoch <maxlimit and abs(x[0]-temp_x0)> 0.00001 and abs(x[1]-temp_x1)>0.00001 and abs(x[2]-temp_x2)>0.00001:
+    temp_x0=x[0]
+    temp_x1=x[1]
+    temp_x2=x[2]
+    for i,w in enumerate(data):
+        t=x[0]*w[0]+x[1]*w[1]+x[2]*w[2]
+        k=temp[i]-t
+        x=x+0.00001*k*w
+    epoch+=1
+
+print(x[0],x[1],x[2])
